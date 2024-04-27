5f2c8fb4afa33d2a5af0b3b309d6cb307ad21af8
Joe <cmksj1005@gmail.com>
2024-04-27 16:33:37 -0400

    c65

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 all_posts = [
     {
         "slug": "hike-in-the-mountains",
-        "image": "SJK.jpg",
+        "image": "test.jpg",
         "author": "Maximilian",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
@@ -81,7 +81,12 @@ def starting_page(request):
   })
 
 def posts(request):
-  return render(request, "blog/all-posts.html")
+  return render(request, "blog/all-posts.html", {
+    "all_posts": all_posts
+  })
 
 def post_detail(request, slug):
-  return render(request, "blog/post-detail.html")
+  identified_post = next(post for post in all_posts if post['slug'] == slug)
+  return render(request, "blog/post-detail.html", {
+    "post": identified_post
+  })
