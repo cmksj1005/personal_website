ed0265fb2f85ffb5376346e59b781bb80336c337
Joe <cmksj1005@gmail.com>
2024-04-30 21:34:38 -0400

    c197

--- blog/views.py
+++ blog/views.py
@@ -54,3 +54,7 @@ class SinglePostView(View):
       "comments": post.comments.all().order_by("-id")
     }
     return render(request, "blog/post-detail.html", context)
+  
+class ReadLaterView(View):
+  def post(self, request):
+    pass
